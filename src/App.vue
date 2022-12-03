f4e8785a92ce24c985898663463ab344e66084b1
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-12-03 20:26:23 +0700

    chore: setup absolute path

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
+<script setup>
+import HelloWorld from "~/components/HelloWorld.vue";
+</script>
+
 <template>
   <div>
+    <HelloWorld />
     <p class="text-7xl">hai there</p>
   </div>
 </template>
