9ae05cb057b2b9db553aba621892561cd752bc0a
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-12-03 22:10:48 +0700

    chore: responsive login page

---
+++ src/components/login/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <form class="mt-20 grid lg:w-[380px]">
+    <span class="font-['Inter'] font-semibold text-[#111827] text-lg"
+      >Sign in to your account</span
+    >
+    <span class="font-['Inter'] mt-3 font-normal text-[#374151] text-sm"
+      >Don’t have an account?
+      <router-link to="register" class="text-[#2563eb] font-medium"
+        >Sign up</router-link
+      >
+      for a free trial.</span
+    >
+    <div class="mt-10">
+      <label
+        for="email"
+        class="font-['Inter'] font-medium text-sm text-[#374151]"
+        >Email address</label
+      >
+      <input
+        required
+        class="appearance-none border rounded w-full py-2.5 px-3 text-gray-900 bg-gray-50 border-gray-200 leading-tight focus:outline-none mt-3 focus:shadow-outline focus:ring focus:ring-blue-500"
+        id="email"
+        type="email"
+      />
+    </div>
+    <div class="mt-8">
+      <label
+        for="password"
+        class="font-['Inter'] font-medium text-sm text-[#374151]"
+        >Password</label
+      >
+      <input
+        required
+        class="appearance-none border rounded w-full py-2.5 px-3 text-gray-900 bg-gray-50 border-gray-200 leading-tight focus:outline-none mt-3 focus:shadow-outline focus:ring focus:ring-blue-500"
+        id="password"
+        type="password"
+      />
+    </div>
+    <button
+      class="group font-['Inter'] inline-flex items-center justify-center rounded-full mt-8 py-2.5 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600 w-full"
+      type="submit"
+    >
+      <span>Sign in <span aria-hidden="true">→</span></span>
+    </button>
+  </form>
+</template>
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,5 +1,24 @@
+<script setup>
+import Logo from "~/components/login/Logo.vue";
+import Form from "~/components/login/Form.vue";
+</script>
+
 <template>
-  <main>
-    <p>this is signin view</p>
+  <main
+    class="p-4 md:p-0 md:bg-[url('https://salient.tailwindui.com/_next/static/media/background-auth.4bcf3f4b.jpg')] md:bg-no-repeat md:bg-cover lg:bg-none lg:grid lg:grid-cols-2 xl:flex"
+  >
+    <div
+      class="mt-6 md:mt-0 md:bg-white sm:w-[420px] md:w-[600px] xl:w-[44%] 2xl:w-1/3 md:h-screen md:grid md:place-content-center sm:mx-auto xl:mx-0"
+    >
+      <Logo />
+      <Form />
+    </div>
+    <div class="hidden lg:flex lg:justify-end xl:w-[56%] 2xl:w-[77vw]">
+      <img
+        src="https://salient.tailwindui.com/_next/static/media/background-auth.4bcf3f4b.jpg"
+        alt=""
+        class="h-screen w-[81%] xl:w-full"
+      />
+    </div>
   </main>
 </template>
